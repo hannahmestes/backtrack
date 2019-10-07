ceceb61ddfcf3ac6db7182f6a94f9b305fdcf96f
Hannah <hannahmestes@gmail.com>
2019-10-07 11:54:35 -0500

    re-started app with tabs

--- backtrack/src/app/app-routing.module.ts
+++ backtrack/src/app/app-routing.module.ts
@@ -2,19 +2,15 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
-  { path: 'find', loadChildren: './pages/find/find.module#FindPageModule' },
-  { path: 'scan', loadChildren: './pages/scan/scan.module#ScanPageModule' },
-  { path: 'settings', loadChildren: './pages/settings/settings.module#SettingsPageModule' },
-  { path: 'resources', loadChildren: './pages/resources/resources.module#ResourcesPageModule' },
-  { path: 'panic', loadChildren: './pages/panic/panic.module#PanicPageModule' },
+  {
+    path: '',
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  }
 ];
-
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ backtrack/src/app/tabs/tabs.page.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tabs',
+  templateUrl: 'tabs.page.html',
+  styleUrls: ['tabs.page.scss']
+})
+export class TabsPage {
+
+  constructor() {}
+
+  panic(){
+    // do the panic thing
+    console.log("Hi");
+  }
+
+}
