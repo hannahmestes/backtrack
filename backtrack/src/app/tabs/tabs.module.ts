4d4d34763c9bc71e79af718d289c062a5e91d738
Wesley Wheeler <wheeler.wes.w@gmail.com>
2019-10-24 18:23:21 -0500

    Sup b*tches

--- backtrack/src/app/tabs/tabs.module.ts
+++ backtrack/src/app/tabs/tabs.module.ts
@@ -2,6 +2,7 @@ import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+//import { VideoPlayer } from '@ionic-native/video-player/ngx';
 
 import { TabsPageRoutingModule } from './tabs.router.module';
 
--- backtrack/src/app/tabs/tabs.page.ts
+++ backtrack/src/app/tabs/tabs.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+//import { VideoPlayer } from '@ionic-native/video-player/ngx';
 
 @Component({
   selector: 'app-tabs',
@@ -7,10 +8,12 @@ import { Component } from '@angular/core';
 })
 export class TabsPage {
 
-  constructor() {}
+  constructor(/*private videoPlayer: VideoPlayer*/) {}
+  runPanic: boolean;
 
   panic(){
     // do the panic thing
+    this.runPanic=true;
     console.log("Hi");
   }
 
