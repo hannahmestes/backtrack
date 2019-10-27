6ca55d316db0356f69c5e11acd9f59e01ef1f886
RobDTK <45511267+RobDTK@users.noreply.github.com>
2019-10-27 15:29:48 -0500

    find-page looks better, still not finished

--- backtrack/src/app/find-page/find-page.page.ts
+++ backtrack/src/app/find-page/find-page.page.ts
@@ -7,42 +7,20 @@ import { ɵTestingCompiler } from '@angular/core/testing';
   styleUrls: ['./find-page.page.scss'],
 })
 export class FindPagePage implements OnInit {
-name: string;
-
-  constructor() {
-   }
-
-  ngOnInit() {
-    
-  }
-
-  public createDevice(id: string){
-    let testing = new device("001");
-    testing.color = colorCircle(testing.distance);
-    console.log(testing.getID());
-    console.log(testing.name);
-    console.log(testing.distance);
-  }
-  public getName(){
-    return "Tile";
-  }
-
-}
-
-export class device {
   id: string;
   distance: number;
   name: string;
   color: number;
-  constructor(id: string) {
-    this.id = id;
-    this.distance = 10; //testing
-    this.name = "Tile"; //testing
-  }
 
-  public getID(){
-    return this.id;
-  }
+  constructor() {
+    this.name = 'Tile';
+    this.distance = 10;
+   }
+
+  ngOnInit() {
+    }
+
+
 }
 
 function colorCircle(distance){
