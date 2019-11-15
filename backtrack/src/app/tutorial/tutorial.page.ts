770bbcb5df9d9331638245950e36e203e85d7686
RobDTK <45511267+RobDTK@users.noreply.github.com>
2019-11-14 19:59:05 -0600

    tutorial is not done but can be access

---
+++ backtrack/src/app/tutorial/tutorial.page.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { TabsPage } from '../tabs/tabs.page';
+import { Storage } from '@ionic/storage';
+import { SettingsService } from '../settings.service';
+
+@Component({
+  selector: 'app-tutorial',
+  templateUrl: './tutorial.page.html',
+  styleUrls: ['./tutorial.page.scss'],
+})
+export class TutorialPage implements OnInit {
+
+  constructor(private router: Router) {
+    SettingsService.hasViewedTutorial = false;
+   }
+
+  ngOnInit() {
+  }
+
+  ReadTutorial() {
+    SettingsService.hasViewedTutorial = true;
+    this.router.navigate(['/tabs/tab1']);
+  }
+
+}
