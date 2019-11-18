684be6989d6b5339cf41b02e081447fceca60b36
Sahi127 <46797317+Sahi127@users.noreply.github.com>
2019-11-18 09:34:45 -0600

    localstuff

--- backtrack/src/app/settings.service.ts
+++ backtrack/src/app/settings.service.ts
@@ -20,26 +20,39 @@ export class SettingsService {
 
   addToWhitelist(trackerAddress: string){
     // add to array and save to local storage
+    
+    
+    this.storage.set("whitelistvalue",JSON.stringify(this.whitelist));
+
   }
 
   addToBlacklist(trackerAddress: string){
     // add to blacklist and save to local storage
+    this.storage.set("blacklistvalue",JSON.stringify(this.blacklist));
   }
 
   acceptTerms(){
     // set accepted terms to true and save to local storage
+    this.storage.set("acceptTerms","true");
   }
 
   tutorialViewed(){
     // set hasViewedTutorial to true and save to local storage
+    this.storage.set("tutorialViewed","true");
   }
 
   setBackgroundScanning(isEnabled: boolean){
     // you get the idea
+    this.storage.set("setBackgroundScanning","true");
   }
 
   retrieveSettings(){
     // get all of the settings from local storage and set them
+    this.storage.get("retrieveSettings","addToBlacklist");
+    this.storage.get("retrienveSettings","acceptTerms");
+    this.storage.get("retrieveSettings","addToWhiteList");
+    this.storage.get("retrieveSettings","tutorialViewed");
+    this.storage.get("retrieveSettings","setBackgroundScanning");
   }
 
 }
